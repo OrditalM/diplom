fec155be8ba2fc57be762527b8f22013e855cd68
OrditalM <denis35822@gmail.com>
2023-11-01 22:56:29 +0200

    Update

--- main.py
+++ main.py
@@ -1,40 +1,51 @@
+import time
+import datetime
+
 import cv2
 
-# Initialize the video capture object
+object_lost = False
 video_file = "./test.mp4"
-cap = cv2.VideoCapture(video_file)  # 0 for the default camera, or provide a video file path
-
-tracker_1 = cv2.TrackerCSRT.create()
+cap = cv2.VideoCapture(video_file)
+fps = 0
+#tracker_1 = cv2.TrackerCSRT.create()
 #tracker_1 = cv2.TrackerKCF.create()
-#tracker_1 = cv2.TrackerMIL.create()
+tracker_1 = cv2.TrackerMIL.create()
 #tracker_1 = cv2.TrackerGOTURN.create()
 #tracker_1 = cv2.TrackerDaSiamRPN.create()
-cursor_x, cursor_y = -1, -1  # Изначально координаты курсора отсутствуют
+cursor_x, cursor_y = -1, -1
 tracking = False
 bbox = None
+desired_fps = 60
 
-# Функция обработчика события мыши
+tracking_time = datetime.datetime.now()
+
+frame_count = 0
+start_time = time.time()
+start_tracking_time = datetime.datetime.now()
+
+cap.set(cv2.CAP_PROP_FPS, 20)
+
 def mouse_callback(event, x, y, flags, param):
     global cursor_x, cursor_y, tracking, bbox
     cv2.rectangle(frame, (x - 20, y - 20), (x + 40, y + 40), (0, 255, 0), 2)
     if event == cv2.EVENT_LBUTTONDOWN:
         cursor_x, cursor_y = x, y
-        bbox = (x - 20, y - 20, 40, 40)  # Создание bounding box 100x100 вокруг клика
+        bbox = (x - 20, y - 20, 40, 40)
         tracking = True
         print("fefss")
 
 cv2.namedWindow('Object Tracking')
 cv2.setMouseCallback('Object Tracking', mouse_callback)
 
-ok = False  # Инициализация переменной ok
+ok = False
 
 while True:
+    timer = cv2.getTickCount()
     ret, frame = cap.read()
     if not ret:
         break
 
     if tracking:
-        # Начать отслеживание объекта
 
         tracker_1.init(frame, bbox)
       #  tracker_2.init(frame, bbox)
@@ -57,24 +68,22 @@ while True:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 0), 2)
 
         else:
-            cv2.putText(frame, "Object Lost", (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+            object_lost = True
+            tracking_time = datetime.datetime.now() - start_tracking_time
+            cv2.putText(frame, f"Object Lost at {datetime.datetime.now() - start_tracking_time} moment", (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
             ok = False
 
     height, width, _ = frame.shape
 
-    # Определение центра кадра
     center_x = width // 2
     center_y = height // 2
 
-    # Размер прицела и рамок
     rect_size = 200
     rect_thickness = 2
 
-    # Определение координат углов прямоугольника для прицела
     top_left = (center_x - rect_size, center_y - rect_size)
     bottom_right = (center_x + rect_size, center_y + rect_size)
 
-    # Рисование прицела и рамок
     cv2.rectangle(frame, top_left, bottom_right, (0, 255, 0), rect_thickness)
     cv2.line(frame, (center_x, center_y - rect_size - 10), (center_x, center_y - rect_size - 5), (0, 255, 0),
              rect_thickness)
@@ -85,16 +94,28 @@ while True:
     cv2.line(frame, (center_x + rect_size + 5, center_y), (center_x + rect_size + 10, center_y), (0, 255, 0),
              rect_thickness)
 
-    # Вывод координат курсора
     cv2.putText(frame, f"Cursor: x={cursor_x}, y={cursor_y}", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
-
+    #cv2.putText(frame, f"KCF TRACKER", (50, 250), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 255), 2)
+    #cv2.putText(frame, f"CSRT TRACKER", (50, 250), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 255), 2)
+    cv2.putText(frame, f"MIL TRACKER", (50, 250), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 255), 2)
+    frame_count += 1
+    if frame_count >= 10:  # Обчислення FPS кожні 10 кадрів
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        fps = frame_count / elapsed_time
+        start_time = end_time
+        frame_count = 0
+    cv2.putText(frame, f"FPS: {fps:.0f}", (50, 150), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+    if object_lost:
+        cv2.putText(frame, f"Object Lost at {tracking_time} moment", (50, 350),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 255), 2)
+    cv2.putText(frame, f"Object Lost at 0:00:26.263743 moment", (50, 350),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 255), 2)
     cv2.imshow('Object Tracking', frame)
 
-    # Press 's' to select a new object to track or 'q' to exit
-    key = cv2.waitKey(1) & 0xFF
+    key = cv2.waitKey(6) & 0xFF
     if key == ord('q'):
         break
 
-# Release the video capture and close all OpenCV windows
 cap.release()
 cv2.destroyAllWindows()
